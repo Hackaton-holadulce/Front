c3d7388f617e50076dcba798b0b5de9d1e15fd2a
AlundraOz <ailuozols@gmail.com>
2020-05-12 19:25:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Home from './Components/Home/Home';
+import { Switch, Route } from 'react-router-dom';
+import Stock from './Components/Stock/Stock';
+import Box from './Components/Box/Box';
 
-function App() {
+
+class App extends React.Component {
+
+
+   render (){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+        <Switch>
+          <Route
+            exact path="/"
+            render={(props) => (
+              <div> <Home  {...props} /> </div>
+                )}
+            />
+         <Route
+              path="/stock"
+            render={(props) => (
+              <div> <Stock  {...props} /> </div>
+                )}
+            /> 
+            <Route
+              path="/box"
+            render={(props) => (
+              <div> <Box  {...props} /> </div>
+                )}
+            />
+          
+          </Switch>
+       </div>
   );
+ }
 }
 
 export default App;
