fe33cbcf5c67f88e8babd35e59e763a900039cc3
Rut <rut.abello@gmail.com>
2020-05-13 02:26:10 +0200

    Installed and used moment for dates

--- src/Components/Stock/Stock.js
+++ src/Components/Stock/Stock.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useContext, useEffect } from 'react';
+import moment from 'moment'
 import { MyContext } from '../context/MyProvider';
 import './Stock.css';
 
@@ -11,55 +12,72 @@ const Stock = () => {
      console.log('productos', state.products)
 
      useEffect(() => {
-          async function fetchMyAPI () {
-               let response = await fetch(`http://localhost:5000/stock_ingredients`)
-               response = await response.json()
-               console.log('response', response)
-               putStockInContext(response)
-          }
+          // const fetchMyAPI = async () => {
+          //      let response = await fetch(`http://localhost:5000/stock_ingredients`)
+          //      response = await response.json()
+          //      console.log('response', response)
+          //      putStockInContext(response)
+          // }
           fetchMyAPI()
      }, [])
+
+     const fetchMyAPI = async () => {
+          let response = await fetch(`http://localhost:5000/stock_ingredients`)
+          response = await response.json()
+          handleExpiration(response)
+          putStockInContext(response)
+     }
+
+     const handleExpiration = (products) => {
+          products.forEach((product) => {
+               const expirationDate = moment(product.expiration_date);
+               product.daysToExpire = -1 * Math.ceil(moment.duration(moment().diff(expirationDate)).as('days'))
+          })
+     }
 
      // const [dateOne, setDateOne] = useState('')
      // const [dateTwo, setDateTwo] = useState('')
 
-     const convert = (e) =>{
-          var date = new Date(e),
-          mnth = ("0" + (date.getMonth() + 1)).slice(-2),
-          day = ("0" + date.getDate()).slice(-2);
-          console.log('mes de la fecha que le pasamos', mnth)
-          // setDateOne([day, mnth, date.getFullYear()].join("/"))
-          return [day, mnth, date.getFullYear()].join("/");
-     }
-     console.log('concrete date converted from yyyy-mm-dd to dd/mm/yyyy', convert('2020-12-19T23:00:00.000Z'))
+     // const convert = (e) =>{
 
-     const getTodaysDate = () =>{
-          var date = new Date(),
-          mnth = ("0" + (date.getMonth() + 1)).slice(-2),
-          day = ("0" + date.getDate()).slice(-2);
-          console.log('mes actual', mnth)
-          // setDateTwo([day, mnth, date.getFullYear()].join("/"))
-          return [day, mnth, date.getFullYear()].join("/");
-     }
-     console.log('todays date', getTodaysDate())
+     //      console.log('date', e)
 
+     //      console.log('parsed', moment(e))
 
-     // // new Date("dateString") is browser-dependent and discouraged, so we'll write
-     // // a simple parse function for U.S. date format (which does no error checking)
-     // function parseDate(str) {
-     //      var mdy = str.split('/');
+     //      return moment(`${e}`)
+     //      // var date = new Date(e),
+     //      // mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+     //      // day = ("0" + date.getDate()).slice(-2);
+     //      // console.log('mes de la fecha que le pasamos', mnth)
+     //      // setDateOne([day, mnth, date.getFullYear()].join("/"))
+     //      // return [day, mnth, date.getFullYear()].join("/");
+     // }
+     // console.log('concrete date converted from yyyy-mm-dd to dd/mm/yyyy', convert('2020-12-19T23:00:00.000Z'))
+
+     // const getTodaysDate = () =>{
+     //      var date = new Date(),
+     //      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+     //      day = ("0" + date.getDate()).slice(-2);
+     //      console.log('mes actual', mnth)
+     //      setDateTwo([day, mnth, date.getFullYear()].join("/"))
+     //      return [day, mnth, date.getFullYear()].join("/");
+     // }
+     // console.log('todays date', getTodaysDate())
+
+     // new Date("dateString") is browser-dependent and discouraged, so we'll write
+     // a simple parse function for U.S. date format (which does no error checking)
+     // const parseDate = (str) => {
+     //      const mdy = str.split('/');
      //      return new Date(mdy[2], mdy[0]-1, mdy[1]);
      // }
 
-     // function datediff(dateOne, dateTwo) {
+     // const datediff = (dateOne, dateTwo) => {
      //      // Take the difference between the dates and divide by milliseconds per day.
      //      // Round to nearest whole number to deal with DST.
      //      return Math.round((dateTwo-dateOne)/(1000*60*60*24));
      // }
 
      // alert(datediff(parseDate(dateOne), parseDate(dateTwo)));
-
-
 
      return (
           <Fragment>
@@ -74,11 +92,13 @@ const Stock = () => {
                <div className="stock-container">
                     {products.map((product, index) => (
 
-                         <div className={product.kg <= 1 ? "empty" : "full"}  key={index}>
+                         <div className={product.kg <= 1 || product.daysToExpire <= 30  ? "empty" : "full"}  key={index}>
                               {product.name}
-                              {` - `}
-                              {convert(product.expiration_date)}
-                              {` - `}
+                              {` || `}
+                              {moment(product.expiration_date).format('L')}
+                              {` || `}
+                              {product.daysToExpire}
+                              {` || `}
                               {product.kg}
                          </div>
                     ))}
