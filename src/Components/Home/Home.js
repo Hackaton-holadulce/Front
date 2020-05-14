25b4722dc5c0b3d6db16812fc5264ba04e56858d
AlundraOz <ailuozols@gmail.com>
2020-05-14 11:40:50 +0200

    dashboard in home , navbar eliminada, links to dashboard in all routes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext, useEffect } from 'react';
 import { MyContext } from '../context/MyProvider';
-import moment from 'moment'
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import logoHola from './logoHolaDulce.png';
 import './Home.css';
+import './Navbar.css'
 
 
 
@@ -36,9 +39,32 @@ const Home = () => {
     return (
         <div className="home-container">
             <div className="main-page">
-                <div className="notification is-danger is-light">
+               { /*<div className="notification is-danger is-light">
                     <button className="delete"></button>
                     {`Tienes `}<strong>{`${products.filter(({ daysToExpire }) => daysToExpire <= 30).length}`}</strong>{` producto(s) a punto de caducar y te estás quedando sin `}<strong>{`${products.filter(({ kg }) => kg <= 1).length}`}</strong>{` producto(s)`}
+                     </div>*/}
+                    <div className="home-play-buttons">
+                     <Link to="stock"> 
+                        <button type="button" className="">
+                            Productos
+                        </button>
+                     </Link>
+                     <Link to="form-stock">
+                        <button type="button" className="" >
+                            Añadir Productos
+                        </button>
+                     </Link>
+                    <Link to="sale">
+                        <button type="button" className="" >
+                            Boxes
+                        </button>
+                    </Link>
+                    <Link to="box">
+                        <button type="button" className="" >
+                            Crear Boxes
+                        </button>
+                    </Link>
+                   
                 </div>
             </div>
         </div>
