0699c03d8ded0a6542509fff8c3a12c27591f16c
mireiaGomariz <mireiapgomariz@gmail.com>
2020-05-14 10:05:28 +0200

    small changes

--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -6,6 +6,7 @@ function Sale() {
     name: "",
     description: "",
     id_box: 0,
+    package: 0,
   }])
   const [display, setDisplay] = useState(false)
 
@@ -16,52 +17,57 @@ function Sale() {
         .then(data => setBox([...data]))
         }, [])
 
-        const submitInfo = (event) => {
+        const buyBox = (event) => {
             event.preventDefault();
+            console.log(event.target.value)
                 fetch('http://localhost:5000/sales', {
                     method: 'POST',
                     headers: new Headers({
                         'Content-Type': 'application/json'
                     }),
                     body: JSON.stringify({
-                      id_sales: box.id_sales,
-                      id_box: box.id_box
+                      id_box: box.id_box,
+                      package: box.package
                     })
                 })
                 .then(res => res.json())
-                .then(res => setDisplay(true))
+                .then(res => console.log(res))
                     setTimeout(() => {
                         setDisplay(false);
                     }, 5000);
-        }
+                  }
 
       return (
         <div>
           <h1>Cajas</h1>
             <div>
             {box.map((type, index) => (
-              <div class="card">
-                <div class="card-image">
-                  <figure class="image image is-3by2">
+              <div className="card">
+                <div className="card-image">
+                  <figure className="image image is-3by2">
                     <img className="img-box" src="https://bulma.io/images/placeholders/480x320.png" alt="Placeholder image"/>
                   </figure>
                 </div>
-                <div class="card-content">
-                  <div class="media">
-                    <div class="media-left">
-                      <figure class="image is-48x48">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image is-48x48">
                         <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
                       </figure>
                     </div>
-                    <div class="media-content">
-                      <p class="title is-4">{type.name}</p>
-                      <p class="subtitle is-6">Baking box: {type.id_box}</p>
+                    <div className="media-content">
+                      <p className="title is-4">{type.name}</p>
+                      <p className="subtitle is-6">Baking box: {type.id_box}</p>
                     </div>
                   </div>
 
-                  <div class="content">
+                  <div className="content">
                     {type.description}
                   </div>
+                  <button
+                    onClick={(event) => buyBox(event)}
+                    value={type.id_box}
+                    className="button is-primary">Comprar</button>
                 </div>
               </div>
             ))}
--- src/Components/Stock/Stock.js
+++ src/Components/Stock/Stock.js
@@ -1,11 +1,35 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import moment from 'moment'
 import { MyContext } from '../context/MyProvider';
 import './Stock.css';
 
 const Stock = () => {
 
-     const { products } = useContext(MyContext)
+  const { putStockInContext, products } = useContext(MyContext);
+
+  useEffect(() => {
+      // const fetchMyAPI = async () => {
+      //      let response = await fetch(`http://localhost:5000/stock_ingredients`)
+      //      response = await response.json()
+      //      console.log('response', response)
+      //      putStockInContext(response)
+      // }
+      fetchMyAPI()
+  }, [])
+
+  const fetchMyAPI = async () => {
+      let response = await fetch(`http://localhost:5000/stock_ingredients`)
+      response = await response.json()
+      handleExpiration(response)
+      putStockInContext(response)
+  }
+
+  const handleExpiration = (products) => {
+      products.forEach((product) => {
+          const expirationDate = moment(product.expiration_date);
+          product.daysToExpire = -1 * Math.ceil(moment.duration(moment().diff(expirationDate)).as('days'))
+      })
+  }
 
      return (
           <Fragment >
